7e984ba0e6233524d5a88cffa21b5078faad0da5
yuki <yukichen@gmx.com>
2026-08-11 15:21:17 +0800

    refactor(desktop): mount RouterProvider in main.tsx

--- apps/desktop/aegis-desktop/src/main.tsx
+++ apps/desktop/aegis-desktop/src/main.tsx
@@ -2,15 +2,24 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import { AegisI18nProvider } from "@aegis/ui/i18n";
 import { AegisThemeProvider } from "@aegis/ui/theme";
+import { createRouter, RouterProvider } from "@tanstack/react-router";
+import { routeTree } from "./routes/routeTree.gen";
 import { DocumentLangSync } from "./DocumentLangSync";
-import App from "./App";
+
+const router = createRouter({ routeTree });
+
+declare module "@tanstack/react-router" {
+  interface Register {
+    router: typeof router;
+  }
+}
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
     <AegisThemeProvider>
       <AegisI18nProvider>
         <DocumentLangSync />
-        <App />
+        <RouterProvider router={router} />
       </AegisI18nProvider>
     </AegisThemeProvider>
   </React.StrictMode>,
